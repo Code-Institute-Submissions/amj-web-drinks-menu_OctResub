3bd5ce8d2bfe2dbf3e4e01b0cf504862e921821b
amj-web <andrewmjarrett@gmail.com>
2022-09-20 01:18:44 +0000

    Change the excerpt to method(how to make the drink) and the contents to ingredients

--- blog/forms.py
+++ blog/forms.py
@@ -12,8 +12,16 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         exclude = ['author', 'updated_on', 'created_on', 'likes', 'slug']
+        labels = {
+            'excerpt': 'Method',
+            'content': 'Ingredients'
+        }
 
 class UpdatePostForm(forms.ModelForm):
     class Meta:
         model = Post
         exclude = []
+        labels = {
+            'excerpt': 'Method',
+            'content': 'Ingredients'
+        }
